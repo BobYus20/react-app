789f93c2b0a54784af0a3f4930fc8f19bc2d8f42
Suhail <suhail142176@gmail.com>
2026-07-05 15:17:07 +0300

    First commit

---
+++ components/MainContent.jsx
@@ -0,0 +1,14 @@
+export default function MainContent() {
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li className="main-list-item">Was first released in 2013</li>
+                <li className="main-list-item">Was originally created by Jordan Walke</li>
+                <li className="main-list-item">Has well over 200K stars on GitHub</li>
+                <li className="main-list-item">Is maintained by Meta</li>
+                <li className="main-list-item">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="react-logo.png" alt="React logo" className="logo" />
+                <span className="nav-span">ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
